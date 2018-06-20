9a91bec16c860179c21248984d0588beeb8e56eb
ACharLuk <acharluk@gmail.com>
2018-06-20 08:13:50 +0200

    Copy all dotfiles

--- setup.sh
+++ setup.sh
@@ -12,7 +12,9 @@ mkdir -p /tmp/selene_setup && cd /tmp/selene_setup
 echo "Downloading dotfiles_setup"
 git clone --recursive https://github.com/SeleneOS/dotfiles_setup.git
 echo "Copying files to home directory"
-cp -rf dotfiles_setup /home/$USER
+cp -rf dotfiles_setup/* ~
+cp -rf dotfiles_setup/.* ~
+rm -rf ~/.git ~/.seleneos/.git
 
 echo "Adding some color to pacman"
 sudo sed -i "/#Color/c\Color" /etc/pacman.conf
