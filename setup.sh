569db622ba78910995710deecac702c690cb2dcd
ACharLuk <acharluk@gmail.com>
2018-06-27 00:38:28 +0200

    Source custom bash_profile and bashrc

--- setup.sh
+++ setup.sh
@@ -1,25 +1,5 @@
-echo "Installing packages"
+echo "Installing Selene OS packages"
 sh installpackages.sh
 
-echo "Installing system files"
-sudo cp -r etc/* /etc
-
-echo "Creating home directories"
-mkdir -p ~/testing ~/downloads ~/repos/$USER ~/documents ~/music ~/pictures/screenshots ~/videos
-
-echo "Creating temporary folder /tmp/selene_setup"
-mkdir -p /tmp/selene_setup && cd /tmp/selene_setup
-echo "Downloading dotfiles_setup"
-git clone --recursive https://github.com/SeleneOS/dotfiles_setup.git
-echo "Copying files to home directory"
-cp -rf dotfiles_setup/* ~
-cp -rf dotfiles_setup/.* ~
-rm -rf ~/.git ~/.gitmodules ~/README.md ~/.seleneos/.git ~/.seleneos/README.md
-
-echo "Adding some color to pacman"
-sudo sed -i "/#Color/c\Color" /etc/pacman.conf
-
-echo "Disabling password for sudo"
-sudo sed -i "/# %wheel ALL=(ALL) NOPASSWD: ALL/c\%wheel ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
-
-echo -e "\n\nSetup finished! Press Control+D to logout or reboot, then login again!\n"
+echo "\nSetting up Selene OS configuration"
+sh setup_config.sh
---
+++ setup_config.sh
@@ -0,0 +1,29 @@
+echo "Installing system files"
+sudo cp -r etc/* /etc
+
+echo "Creating home directories"
+mkdir -p ~/testing ~/downloads ~/repos/$USER ~/documents ~/music ~/pictures/screenshots ~/videos ~/.seleneos_data
+
+echo "Creating temporary folder /tmp/selene_setup"
+mkdir -p /tmp/selene_setup && cd /tmp/selene_setup
+echo "Downloading dotfiles_setup"
+git clone --recursive https://github.com/SeleneOS/dotfiles_setup.git
+echo "Copying files to home directory"
+cp -rf dotfiles_setup/* ~
+cp -rf dotfiles_setup/.* ~
+rm -rf ~/.git ~/.gitmodules ~/README.md ~/.seleneos/.git ~/.seleneos/README.md
+
+if [[ -f /etc/pacman.conf ]]; then
+    echo "Adding some color to pacman"
+    sudo sed -i "/#Color/c\Color" /etc/pacman.conf
+fi
+
+echo "Disabling password for sudo"
+sudo sed -i "/# %wheel ALL=(ALL) NOPASSWD: ALL/c\%wheel ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
+
+echo "Adding bash_profile and bashrc"
+echo -e "\n# Selene OS\n[[ -f ~/.seleneos/.bash_profile ]] && . ~/.seleneos/.bash_profile" >> ~/.bash_profile
+echo -e "\n[[ -f ~/.seleneos/.bashrc ]] && . ~/.seleneos/.bashrc" >> ~/.bashrc
+
+
+echo -e "\n\nSetup finished! Press Control+D to logout or reboot, then login again!\n"
