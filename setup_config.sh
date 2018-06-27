9d0103d5a9bca9e7569135e737678932cef315b3
ACharLuk <acharluk@gmail.com>
2018-06-27 19:19:24 +0200

    Some more checks

--- setup_config.sh
+++ setup_config.sh
@@ -13,6 +13,9 @@ cp -rf dotfiles_setup/* ~
 cp -rf dotfiles_setup/.* ~
 rm -rf ~/.git ~/README.md
 
+if [[ -d ~/.seleneos ]]; then
+    mv ~/.seleneos ~/.seleneos_old
+fi
 git clone https://github.com/SeleneOS/dotfiles_setup.git ~/.seleneos
 
 if [[ -f /etc/pacman.conf ]]; then
@@ -23,9 +26,14 @@ fi
 echo "Disabling password for sudo"
 sudo sed -i "/# %wheel ALL=(ALL) NOPASSWD: ALL/c\%wheel ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
 
-echo "Adding bash_profile and bashrc"
-echo -e "\n# Selene OS\n[[ -f ~/.seleneos/.bash_profile ]] && . ~/.seleneos/.bash_profile" >> ~/.bash_profile
-echo -e "\n# Selene OS\n[[ -f ~/.seleneos/.bashrc ]] && . ~/.seleneos/.bashrc" >> ~/.bashrc
+if ! grep -q seleneos "/home/$USER/.bash_profile"; then
+    echo "Adding bash_profile"
+    echo -e "\n# Selene OS\n[[ -f ~/.seleneos/.bash_profile ]] && . ~/.seleneos/.bash_profile" >> ~/.bash_profile
+fi
+if ! grep -q seleneos "/home/$USER/.bashrc"; then
+    echo "Adding bashrc"
+    echo -e "\n# Selene OS\n[[ -f ~/.seleneos/.bashrc ]] && . ~/.seleneos/.bashrc" >> ~/.bashrc
+fi
 
 
 echo -e "\n\nSetup finished! Press Control+D to logout or reboot, then login again!\n"
