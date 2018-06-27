6441955145b4eea2f45f18316079e54d7a11156d
ACharLuk <acharluk@gmail.com>
2018-06-27 18:59:33 +0200

    Update setup script to download dotfiles git repo

--- setup_config.sh
+++ setup_config.sh
@@ -7,11 +7,13 @@ mkdir -p ~/testing ~/downloads ~/repos/$USER ~/documents ~/music ~/pictures/scre
 echo "Creating temporary folder /tmp/selene_setup"
 mkdir -p /tmp/selene_setup && cd /tmp/selene_setup
 echo "Downloading dotfiles_setup"
-git clone --recursive https://github.com/SeleneOS/dotfiles_setup.git
+git clone https://github.com/SeleneOS/dotfiles_setup.git
 echo "Copying files to home directory"
 cp -rf dotfiles_setup/* ~
 cp -rf dotfiles_setup/.* ~
-rm -rf ~/.git ~/.gitmodules ~/README.md
+rm -rf ~/.git ~/README.md
+
+git clone https://github.com/SeleneOS/dotfiles_setup.git ~/.seleneos
 
 if [[ -f /etc/pacman.conf ]]; then
     echo "Adding some color to pacman"
